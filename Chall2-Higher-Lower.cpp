2210c520d55dddf5a057d31c6f232584430193fa
Jake Ralph <flooblegoo@gmail.com>
2016-03-01 09:52:35 +1100

    V0.1
    Code started, changed 10 tries to 5 to make it a faster game

--- Chall2-Higher-Lower.cpp
+++ Chall2-Higher-Lower.cpp
@@ -4,11 +4,31 @@
 #include "stdafx.h"
 #include <iostream>
 #include <ctime>
+#include <string>
 
 
 int main()
 {
+	// Generate random number for guess (Between 1 - 100) and declare the
+	// computer's guessed number variable as 'the_number'
+	srand(time(0));
+	int base_random_number = rand();
+	int the_number = (base_random_number % 100) + 1;
 
+	// Declare  all other variables
+	int tries_used = 0;
+	int tries_left = 5;
+	int player_guess;
+	std::string player_restart;
+
+	// Begin game loop
+	while (tries_left > 0)
+	{
+		// Check tries remaining
+		tries_left = tries_left - tries_used;
+
+
+	}
 
     return 0;
 }
